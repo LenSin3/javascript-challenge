9f26b67fb30c22f856c0073ec8a06bd129d77398
LenSin3 <lenpk3@yahoo.co.uk>
2020-11-24 02:11:01 -0800

    Update code to populate initial table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,17 +1,53 @@
 // from data.js
 var tableData = data;
 
+// Extract data for the table columns `date/time`, `city`, `state`, `country`, `shape`, and `comment`
+var dataList = []
+data.map(element => {
+    dataList.push(element.datetime);
+    
+    dataList.push(element.city);
+    dataList.push(element.state);
+    dataList.push(element.country);
+    dataList.push(element.shape);
+    dataList.push(element.comment);
+    
+});
+
+dataList.forEach(element => {console.table(element);});
+
 // YOUR CODE HERE!
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
 // // Step 1: Loop Through `data` and console.table each UFO sighting
 data.forEach(element => {
-    console.table(element);
+    
     // // Append 'tr' elements to the tbody
     var row = tbody.append('tr');
     // // Use `Object.entries` to console.log each UFO Sighting
-    Object.entries(data).forEach((key, value) => console.log([key, value]));
-    // // Append cell to each row for each value
-    var cell = row.append("td");
+    Object.entries(element).forEach((key, value) => {
+
+
+        var cell = row.append("td");
+        // update each cell text with UFO sighting values `date/time`, `city`, `state`, `country`, `shape`, and `comment`
+        cell.text(value);
+
+
+    }
+    
+    
+    );
+    
+
 });
+
+
+/* // BONUS: Refactor to use Arrow Functions!
+data.forEach((weatherReport) => {
+  var row = tbody.append("tr");
+  Object.entries(weatherReport).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+}); */
