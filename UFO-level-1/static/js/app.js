3ee7fe54417ae3a6b13f7e74ee730ebff0b4ef46
LenSin3 <lenpk3@yahoo.co.uk>
2020-11-24 01:10:32 -0800

    Step 1 completed. Loop through data and console.table each UFO sighting

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,10 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// // Step 1: Loop Through `data` and console.table each UFO sighting
+data.forEach(element => {
+    console.table(element);
+});
