adb9f94182de0f2b7d3347420de6a7d6e1e1b4e8
LenSin3 <lenpk3@yahoo.co.uk>
2020-11-24 01:25:11 -0800

    Append cell to each row per value

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -10,5 +10,8 @@ data.forEach(element => {
     console.table(element);
     // // Append 'tr' elements to the tbody
     var row = tbody.append('tr');
-    
+    // // Use `Object.entries` to console.log each UFO Sighting
+    Object.entries(data).forEach((key, value) => console.log([key, value]));
+    // // Append cell to each row for each value
+    var cell = row.append("td");
 });
