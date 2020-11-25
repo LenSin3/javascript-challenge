a53c8718e96bce8aeb36f46a5a927a3cdc545055
LenSin3 <lenpk3@yahoo.co.uk>
2020-11-24 22:32:52 -0800

    Completed forms to return data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,90 +1,16 @@
 // from data.js
 var tableData = data;
 
-// Extract data for the table columns `date/time`, `city`, `state`, `country`, `shape`, and `comment`
-// var dataList = []
-
-/* data.forEach((element) => {
-  
-
-  dataList.push(element.datetime);
-  dataList.push(element.city);
-  dataList.push(element.state);
-  dataList.push(element.country);
-  dataList.push(element.shape);
-  dataList.push(element.comments);
-
-
-  
-}); */
 
 var dataList = data.filter(function (e) {
   return (e.datetime || e.city || e.state || e.country || e.shape || e.comments);
 })
 console.table(dataList);
 var tbody =  d3.select("tbody");
-/* data.map(element => {
-    dataList.push(datetime);
-    dataList.push(city);
-    dataList.push(state);
-    dataList.push(country);
-    dataList.push(shape);
-    dataList.push(comments);
-    
-});
-console.table(dataList);
-// Get a reference to the table body
-var tbody = d3.select("tbody");
-
-/* dataList.forEach(function(element) {
-  console.log(element);
-  
-   // // Append 'tr' elements to the tbody
-   var row = tbody.append('tr');
-   // // Use `Object.entries` to console.log each UFO Sighting
-   Object.entries(element).forEach(function([key, value]) {
-
-    console.log(key, value);
-
-    var cell = row.append("td");
-       
-       // update each cell text with UFO sighting values `date/time`, `city`, `state`, `country`, `shape`, and `comment`
-    cell.text(value);
-
-
-   });
- });
-*/
-// dataList.forEach(element => {console.table(element);});
-
-// YOUR CODE HERE!
-// Get a reference to the table body
-// var tbody = d3.select("tbody");
-
-// // Step 1: Loop Through `data` and console.table each UFO sighting
-// dataList.forEach(element => {
-    
-    // // Append 'tr' elements to the tbody
-    // var row = tbody.append('tr');
-    // // Use `Object.entries` to console.log each UFO Sighting
-    // Object.entries(element).forEach(([key, value]) => {
-
-
-        // var cell = row.append("td");
-        // update each cell text with UFO sighting values `date/time`, `city`, `state`, `country`, `shape`, and `comment`
-        // cell.text(value);
-
-
-    // }
-    
-    
-    // );
-    
-
-// });
 
 
  // BONUS: Refactor to use Arrow Functions!
+/*
 dataList.forEach((element) => {
   var row = tbody.append("tr");
   Object.entries(element).forEach(([key, value]) => {
@@ -92,3 +18,110 @@ dataList.forEach((element) => {
     cell.text(value);
   });
 }); 
+*/
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.selectAll("#form");
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+var inputValue = "";
+
+// Create the function to run for both events
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+
+  var inputElementDate = d3.select("#datetime");
+  var inputElementCity = d3.select("#city");
+  var inputElementState = d3.select("#state");
+  var inputElementCountry = d3.select("#country");
+  var inputElementShape = d3.select("#shape");
+
+ 
+
+
+  // Get the value property of the input element
+  if (inputElementDate) {
+     inputValue = inputElementDate.property("value");
+     console.log(inputValue);
+     var filteredData = dataList.filter(element => element.datetime === inputValue);
+     console.log(filteredData);
+
+     filteredData.forEach((element) => {
+      var row = tbody.append("tr");
+      Object.entries(element).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+  };
+
+  if (inputElementCity) {
+    inputValue = inputElementCity.property("value");
+    console.log(inputValue);
+    var filteredData = dataList.filter(element => element.city === inputValue);
+    console.log(filteredData);
+
+    filteredData.forEach((element) => {
+      var row = tbody.append("tr");
+      Object.entries(element).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+  };
+
+  if (inputElementState) {
+    inputValue = inputElementState.property("value");
+    console.log(inputValue);
+    var filteredData = dataList.filter(element => element.state === inputValue);
+    console.log(filteredData);
+
+    filteredData.forEach((element) => {
+      var row = tbody.append("tr");
+      Object.entries(element).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+  };
+
+  if (inputElementCountry) {
+    inputValue = inputElementCountry.property("value");
+    console.log(inputValue);
+    var filteredData = dataList.filter(element => element.country === inputValue);
+    console.log(filteredData);
+
+    filteredData.forEach((element) => {
+      var row = tbody.append("tr");
+      Object.entries(element).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+  };
+
+  if (inputElementShape) {
+    inputValue = inputElementShape.property("value");
+    console.log(inputValue);
+    var filteredData = dataList.filter(element => element.shape === inputValue);
+    console.log(filteredData);
+
+    filteredData.forEach((element) => {
+      var row = tbody.append("tr");
+      Object.entries(element).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+  };
+}
